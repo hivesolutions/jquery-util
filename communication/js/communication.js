e01e3a95147707f79bb0529825cfb636b394410c
joamag <joamag@gmail.com>
2013-07-05 19:45:41 +0100

    better support for async under the uxf

--- communication/js/communication.js
+++ communication/js/communication.js
@@ -171,6 +171,14 @@
          * Registers the event handlers for the created objects.
          */
         var _registerHandlers = function() {
+            // in case the matched object is not defined
+            // or in case it's an empty list must return
+            // immediatly initialization is not meant to
+            // be run (corruption may occur)
+            if (!matchedObject || matchedObject.length == 0) {
+                return;
+            }
+
             // retrieves the various options that were provided
             // to configure the current matched object
             var url = options["url"];
